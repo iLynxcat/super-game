ac73376ce646ba4c7ac37fe10df965b05411146f
Lynx <lynx@ilynxcat.me>
2026-01-07 18:59:34 -0800

    use deltaTime for cube movement

--- Sources/SuperGame/Objects/PlayerCube.swift
+++ Sources/SuperGame/Objects/PlayerCube.swift
@@ -2,21 +2,23 @@ import Raylib
 
 class PlayerCube: GameObject, Positionable {
     let size: Float = 50.0
-    let speed: Float = 2.0
+    let speed: Float = 100.0  // units per second
     var position = Vector3(x: 0, y: 25, z: 0)
 
     func update(deltaTime: Float) {
+        let movement = speed * deltaTime
+
         if IsKeyDown(KEY_W.key) {
-            position.z -= speed
+            position.z -= movement
         }
         if IsKeyDown(KEY_S.key) {
-            position.z += speed
+            position.z += movement
         }
         if IsKeyDown(KEY_A.key) {
-            position.x -= speed
+            position.x -= movement
         }
         if IsKeyDown(KEY_D.key) {
-            position.x += speed
+            position.x += movement
         }
     }
 
