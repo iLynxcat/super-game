f5cd17c946ee77fad763538860f2f82af05e3b9c
Lynx <lynx@ilynxcat.me>
2026-01-09 20:05:52 -0800

    make ground plane orange and larger

--- Sources/Game/Objects/Backdrop.swift
+++ Sources/Game/Objects/Backdrop.swift
@@ -1,10 +1,14 @@
 import Engine
 
+extension Color {
+    fileprivate static let groundColor = Color(255, 128, 0)
+}
+
 public class Backdrop: Drawable {
     public var layer: Int { DrawLayer.world.rawValue }
 
     public func draw(context: DrawContext) {
         context.clear(color: .black)
-        context.drawPlane(center: .zero, size: Vector2(20, 20), color: .white)
+        context.drawPlane(center: .zero, size: Vector2(64, 64), color: .groundColor)
     }
 }
